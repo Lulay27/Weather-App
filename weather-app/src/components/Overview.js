b27b5eaf7b0ae78ebd376b1b271e5186e962b593
EricLe27 <ericle201727@gmail.com>
2022-08-17 01:03:31 -0700

    moved all 3 api calls within submit handler and created overview to render if onsubmit is true

--- weather-app/src/components/Overview.js
+++ weather-app/src/components/Overview.js
@@ -5,87 +5,37 @@ export default class Overview extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      city: '',
-      coord: {
-        long: 0,
-        lat: 0,
-      },
-      weather: {
-        temp: '',
-        feel: '',
-        hum: '',
-        desc: '',
-        country: '',
-      },
-    };
+    // this.state = {
+    //   city: '',
+    //   coord: {
+    //     long: 0,
+    //     lat: 0,
+    //   },
+    //   weather: {
+    //     temp: '',
+    //     feel: '',
+    //     hum: '',
+    //     desc: '',
+    //     country: '',
+    //   },
+    // };
   }
-
-  handleButtonClick = () => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${this.props.gifSearch}&appid=8371ba1206036d8bad7d681b9fced4bd`,
-      { mode: 'cors' }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        console.log(response[0].country);
-        this.setState({
-          coord: {
-            long: response[0].lon,
-            lat: response[0].lat,
-          },
-          weather: {
-            country: response[0].country,
-          },
-        });
-      });
-
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.state.coord.lat}&lon=${this.state.coord.long}&appid=8371ba1206036d8bad7d681b9fced4bd`,
-      { mode: 'cors' }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          weather: {
-            temp: response.main.temp,
-            feel: response.main.feels_like,
-            hum: response.main.humidity,
-            desc: response.weather[0].description,
-            country: this.state.weather.country,
-          },
-        });
-      });
-
-    // return (
-    //   <li>
-    //     {this.props.gifSearch} {this.state.weather.temp}{' '}
-    //     {this.state.weather.feel}
-    //   </li>
-    // );
-  };
 
   // is there a way to do this differently? like return jsx on handlebuttonclick() call?
 
   render() {
     return (
-      <div>
-        {this.props.isSubmitting ? this.handleButtonClick() : ''}
-        {/* <button onClick={this.handleButtonClick}>destroy all plants</button> */}
+      <>
         <div>
-          <li>{this.props.gifSearch}</li>
-          <li>{this.state.weather.temp}</li>
-          <li>{this.state.weather.feel}</li>
-          <li>{this.state.weather.hum}</li>
-          <li>{this.state.weather.desc}</li>
-          <li>{this.state.weather.country}</li>
+          <li>
+            Location: {this.props.gifSearch},{this.props.weather.country}
+          </li>
+          <li>Temperature: {this.props.weather.temp}</li>
+          <li>Feels like: {this.props.weather.feel}</li>
+          <li>Humidty: {this.props.weather.hum}</li>
+          <li>Forcast: {this.props.weather.desc}</li>
         </div>
-      </div>
+      </>
     );
   }
 }
--- weather-app/src/components/SearchBar.js
+++ weather-app/src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GifAPI from './GifAPI';
 import Overview from './Overview';
+// import Overview from './Overview';
 
 // right now searchbar is the overview mode code over and clean it so logic makes sense
 
@@ -13,6 +14,18 @@ export class SearchBar extends Component {
       gifSearch: '',
       isSubmitting: false,
       imgSrc: '',
+      city: '',
+      coord: {
+        long: 0,
+        lat: 0,
+      },
+      weather: {
+        temp: '',
+        feel: '',
+        hum: '',
+        desc: '',
+        country: '',
+      },
     };
   }
 
@@ -29,11 +42,9 @@ export class SearchBar extends Component {
       gifSearch: this.state.input,
       isSubmitting: true,
     });
-  };
 
-  handleApiGif = (gifSearch) => {
     fetch(
-      `https://api.giphy.com/v1/gifs/translate?api_key=Y3N6ACeh4OhO4q5qG0Z7Sgbk5KIrzRSY&s=${gifSearch}`,
+      `https://api.giphy.com/v1/gifs/translate?api_key=Y3N6ACeh4OhO4q5qG0Z7Sgbk5KIrzRSY&s=${this.state.input}`,
       { mode: 'cors' }
     )
       .then((response) => {
@@ -42,14 +53,152 @@ export class SearchBar extends Component {
       .then((response) => {
         this.setState({
           imgSrc: response.data.images.original.url,
-          isSubmitting: false,
+          // isSubmitting: false,
+        });
+      });
+
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${this.state.input}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+      { mode: 'cors' }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response[0].country);
+        this.setState({
+          coord: {
+            long: response[0].lon,
+            lat: response[0].lat,
+          },
+          weather: {
+            country: response[0].country,
+          },
+        });
+      });
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.state.coord.lat}&lon=${this.state.coord.long}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+      { mode: 'cors' }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          // isSubmitting: false,
+          weather: {
+            temp: response.main.temp,
+            feel: response.main.feels_like,
+            hum: response.main.humidity,
+            desc: response.weather[0].description,
+            country: this.state.weather.country,
+          },
         });
       });
   };
 
+  handleApiGif = (gifSearch) => {
+    // fetch(
+    //   `https://api.giphy.com/v1/gifs/translate?api_key=Y3N6ACeh4OhO4q5qG0Z7Sgbk5KIrzRSY&s=${gifSearch}`,
+    //   { mode: 'cors' }
+    // )
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((response) => {
+    //     this.setState({
+    //       imgSrc: response.data.images.original.url,
+    //       isSubmitting: false,
+    //     });
+    //   });
+    // fetch(
+    //   `http://api.openweathermap.org/geo/1.0/direct?q=${gifSearch}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+    //   { mode: 'cors' }
+    // )
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((response) => {
+    //     console.log(response[0].country);
+    //     this.setState({
+    //       coord: {
+    //         long: response[0].lon,
+    //         lat: response[0].lat,
+    //       },
+    //       weather: {
+    //         country: response[0].country,
+    //       },
+    //     });
+    //   });
+    // fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.state.coord.lat}&lon=${this.state.coord.long}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+    //   { mode: 'cors' }
+    // )
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //     this.setState({
+    //       isSubmitting: false,
+    //       weather: {
+    //         temp: response.main.temp,
+    //         feel: response.main.feels_like,
+    //         hum: response.main.humidity,
+    //         desc: response.weather[0].description,
+    //         country: this.state.weather.country,
+    //       },
+    //     });
+    //   });
+  };
+
+  handleApiWeather = (gifSearch) => {
+    // fetch(
+    //   `http://api.openweathermap.org/geo/1.0/direct?q=${gifSearch}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+    //   { mode: 'cors' }
+    // )
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((response) => {
+    //     console.log(response[0].country);
+    //     this.setState({
+    //       coord: {
+    //         long: response[0].lon,
+    //         lat: response[0].lat,
+    //       },
+    //       weather: {
+    //         country: response[0].country,
+    //       },
+    //     });
+    //   });
+    // fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.state.coord.lat}&lon=${this.state.coord.long}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+    //   { mode: 'cors' }
+    // )
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //     this.setState({
+    //       isSubmitting: false,
+    //       weather: {
+    //         temp: response.main.temp,
+    //         feel: response.main.feels_like,
+    //         hum: response.main.humidity,
+    //         desc: response.weather[0].description,
+    //         country: this.state.weather.country,
+    //       },
+    //     });
+    //   });
+  };
+
   render() {
     return (
-      <div>
+      <>
         <div>
           <form onSubmit={this.handleSubmit}>
             <input
@@ -63,23 +212,20 @@ export class SearchBar extends Component {
         </div>
 
         <div>
-          <GifAPI
-            isSubmitting={this.state.isSubmitting}
-            handleApiGif={this.handleApiGif}
-            gifSearch={this.state.gifSearch}
-          />
-        </div>
-        <div>
-          <img src={this.state.imgSrc}></img>
+          <img alt="" src={this.state.imgSrc}></img>
         </div>
 
         <div>
-          <Overview
-            isSubmitting={this.state.isSubmitting}
-            gifSearch={this.state.gifSearch}
-          />
+          {this.state.isSubmitting ? (
+            <Overview
+              weather={this.state.weather}
+              isSubmitting={this.state.isSubmitting}
+            />
+          ) : (
+            ''
+          )}
         </div>
-      </div>
+      </>
     );
   }
 }
