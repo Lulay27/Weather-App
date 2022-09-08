d3615426d86f8a5f9bc66bd5a504ae9035779381
EricLe27 <ericle201727@gmail.com>
2022-09-07 17:25:59 -0700

    fixed problem. NOTE never setstate and try to use new state before rendering, b/c although setstate has run state is not updated until rendering

--- weather-app/src/components/SearchBar.js
+++ weather-app/src/components/SearchBar.js
@@ -38,23 +38,6 @@ export class SearchBar extends Component {
       isSubmitting: true,
     });
 
-    console.log('gifsearch:', this.state.gifSearch);
-    console.log('input:', this.state.input);
-
-    // fetch(
-    //   `https://api.giphy.com/v1/gifs/translate?api_key=Y3N6ACeh4OhO4q5qG0Z7Sgbk5KIrzRSY&s=${this.state.input}`,
-    //   { mode: 'cors' }
-    // )
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((response) => {
-    //     this.setState({
-    //       imgSrc: response.data.images.original.url,
-    //       // isSubmitting: false,
-    //     });
-    //   });
-
     const cordsRes = await (
       await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${this.state.input}&appid=8371ba1206036d8bad7d681b9fced4bd`,
@@ -62,59 +45,22 @@ export class SearchBar extends Component {
       )
     ).json();
 
-    this.setState({
-      coord: {
-        long: cordsRes[0].lon,
-        lat: cordsRes[0].lat,
-      },
-      weather: {
-        country: cordsRes[0].country,
-      },
-    });
-
-    console.log('Coords:', cordsRes);
-
-    // fetch(
-    //   `http://api.openweathermap.org/geo/1.0/direct?q=${this.state.input}&appid=8371ba1206036d8bad7d681b9fced4bd`,
-    //   { mode: 'cors' }
-    // )
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((response) => {
-    //     console.log('coords', response);
-    //     this.setState({
-    //       coord: {
-    //         long: response[0].lon,
-    //         lat: response[0].lat,
-    //       },
-    //       weather: {
-    //         country: response[0].country,
-    //       },
-    //     });
-    //   });
-
     const weatherRes = await (
       await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.state.coord.lat}&lon=${this.state.coord.long}&appid=8371ba1206036d8bad7d681b9fced4bd`,
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${cordsRes[0].lat}&lon=${cordsRes[0].lon}&appid=8371ba1206036d8bad7d681b9fced4bd`,
         { mode: 'cors' }
       )
     ).json();
 
     this.setState({
-      // input: '',
       weather: {
         temp: weatherRes.main.temp,
         feel: weatherRes.main.feels_like,
         hum: weatherRes.main.humidity,
         desc: weatherRes.weather[0].description,
-        // country: this.state.weather.country,
+        country: cordsRes[0].country,
       },
     });
-
-    // debuging
-    console.log(this.state.input, weatherRes);
-    console.log(this.state.gifSearch, cordsRes);
   };
 
   render() {
@@ -131,11 +77,6 @@ export class SearchBar extends Component {
             <button>Submit</button>
           </form>
         </div>
-
-        {/* <div>
-          <img alt="" src={this.state.imgSrc}></img>
-        </div> */}
-
         <div>
           <Overview
             isSubmitting={this.state.isSubmitting}
