b65b643e70ec7e9335278e3b8da5647908a25b6c
EricLe27 <ericle201727@gmail.com>
2023-03-04 11:20:40 -0800

    added clear

--- weather-app/src/components/SidePanelData.js
+++ weather-app/src/components/SidePanelData.js
@@ -3,19 +3,14 @@ import styles from './componentStyles.module.css';
 
 export default function WeatherDisplay(props) {
   const latestCity = props.cityArr[props.cityArr.length - 1];
-
   return (
     <>
-      {props.cityArr.length > 0 ? (
-        <div className={styles.sidePanelContainer}>
-          <h2>Weather Statistics</h2>
-          <li>Feels like: {latestCity.main.feels_like}</li>
-          <li>Humidty: {latestCity.main.humidity}</li>
-          <li>Forcast: {latestCity.weather[0].description}</li>
-        </div>
-      ) : (
-        ''
-      )}
+      <div className={styles.sidePanelContainer}>
+        <h2>Weather Statistics</h2>
+        <li>Feels like: {latestCity.main.feels_like}</li>
+        <li>Humidty: {latestCity.main.humidity}</li>
+        <li>Forcast: {latestCity.weather[0].description}</li>
+      </div>
     </>
   );
 }
